97a57b3614508451cba302e6b220473502001124
Axel Mario Carvantes Gutierrez <axelmg20@hotmail.com>
2023-03-13 11:46:39 -0600

    Add project files.

---
+++ Textado/frmTextado.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace Textado
+{
+    partial class frmTextado
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmTextado));
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btnBucar = new System.Windows.Forms.Button();
+            this.ofdSeleccionar = new System.Windows.Forms.OpenFileDialog();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.circularProgress1 = new DevComponents.DotNetBar.Controls.CircularProgress();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.ribbonControl1 = new DevComponents.DotNetBar.RibbonControl();
+            this.ribbonPanel1 = new DevComponents.DotNetBar.RibbonPanel();
+            this.ribbonBar4 = new DevComponents.DotNetBar.RibbonBar();
+            this.btnSalir = new DevComponents.DotNetBar.ButtonItem();
+            this.btnLimpiar = new DevComponents.DotNetBar.RibbonBar();
+            this.btnInicio = new DevComponents.DotNetBar.ButtonItem();
+            this.ribbonTabItem2 = new DevComponents.DotNetBar.RibbonTabItem();
+            this.styleManager1 = new DevComponents.DotNetBar.StyleManager(this.components);
+            this.ribbonControl1.SuspendLayout();
+            this.ribbonPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(36, 129);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(98, 13);
+            this.lblNombre.TabIndex = 0;
+            this.lblNombre.Text = "Documento original";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(148, 127);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.ReadOnly = true;
+            this.txtNombre.Size = new System.Drawing.Size(484, 20);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // btnBucar
+            // 
+            this.btnBucar.Image = ((System.Drawing.Image)(resources.GetObject("btnBucar.Image")));
+            this.btnBucar.Location = new System.Drawing.Point(659, 127);
+            this.btnBucar.Name = "btnBucar";
+            this.btnBucar.Size = new System.Drawing.Size(40, 21);
+            this.btnBucar.TabIndex = 2;
+            this.btnBucar.UseVisualStyleBackColor = true;
+            this.btnBucar.Click += new System.EventHandler(this.btnBucar_Click);
+            // 
+            // ofdSeleccionar
+            // 
+            this.ofdSeleccionar.FileName = "ofdSeleccionar";
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(148, 167);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.ReadOnly = true;
+            this.txtResultado.Size = new System.Drawing.Size(484, 20);
+            this.txtResultado.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 167);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Documento textado";
+            // 
+            // circularProgress1
+            // 
+            // 
+            // 
+            // 
+            this.circularProgress1.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.circularProgress1.Location = new System.Drawing.Point(323, 205);
+            this.circularProgress1.Name = "circularProgress1";
+            this.circularProgress1.ProgressBarType = DevComponents.DotNetBar.eCircularProgressType.Dot;
+            this.circularProgress1.Size = new System.Drawing.Size(84, 38);
+            this.circularProgress1.Style = DevComponents.DotNetBar.eDotNetBarStyle.OfficeXP;
+            this.circularProgress1.TabIndex = 5;
+            this.circularProgress1.Visible = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "█",
+            "░",
+            "▒",
+            "▓"});
+            this.comboBox1.Location = new System.Drawing.Point(659, 167);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(40, 21);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // ribbonControl1
+            // 
+            this.ribbonControl1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(239)))), ((int)(((byte)(242)))));
+            // 
+            // 
+            // 
+            this.ribbonControl1.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.ribbonControl1.Controls.Add(this.ribbonPanel1);
+            this.ribbonControl1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ribbonControl1.ForeColor = System.Drawing.Color.Black;
+            this.ribbonControl1.Items.AddRange(new DevComponents.DotNetBar.BaseItem[] {
+            this.ribbonTabItem2});
+            this.ribbonControl1.KeyTipsFont = new System.Drawing.Font("Tahoma", 7F);
+            this.ribbonControl1.Location = new System.Drawing.Point(0, 0);
+            this.ribbonControl1.Name = "ribbonControl1";
+            this.ribbonControl1.Padding = new System.Windows.Forms.Padding(0, 0, 0, 3);
+            this.ribbonControl1.Size = new System.Drawing.Size(761, 98);
+            this.ribbonControl1.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.ribbonControl1.SystemText.MaximizeRibbonText = "&Maximize the Ribbon";
+            this.ribbonControl1.SystemText.MinimizeRibbonText = "Mi&nimize the Ribbon";
+            this.ribbonControl1.SystemText.QatAddItemText = "&Add to Quick Access Toolbar";
+            this.ribbonControl1.SystemText.QatCustomizeMenuLabel = "<b>Customize Quick Access Toolbar</b>";
+            this.ribbonControl1.SystemText.QatCustomizeText = "&Customize Quick Access Toolbar...";
+            this.ribbonControl1.SystemText.QatDialogAddButton = "&Add >>";
+            this.ribbonControl1.SystemText.QatDialogCancelButton = "Cancel";
+            this.ribbonControl1.SystemText.QatDialogCaption = "Customize Quick Access Toolbar";
+            this.ribbonControl1.SystemText.QatDialogCategoriesLabel = "&Choose commands from:";
+            this.ribbonControl1.SystemText.QatDialogOkButton = "OK";
+            this.ribbonControl1.SystemText.QatDialogPlacementCheckbox = "&Place Quick Access Toolbar below the Ribbon";
+            this.ribbonControl1.SystemText.QatDialogRemoveButton = "&Remove";
+            this.ribbonControl1.SystemText.QatPlaceAboveRibbonText = "&Place Quick Access Toolbar above the Ribbon";
+            this.ribbonControl1.SystemText.QatPlaceBelowRibbonText = "&Place Quick Access Toolbar below the Ribbon";
+            this.ribbonControl1.SystemText.QatRemoveItemText = "&Remove from Quick Access Toolbar";
+            this.ribbonControl1.TabGroupHeight = 14;
+            this.ribbonControl1.TabIndex = 29;
+            // 
+            // ribbonPanel1
+            // 
+            this.ribbonPanel1.ColorSchemeStyle = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.ribbonPanel1.Controls.Add(this.ribbonBar4);
+            this.ribbonPanel1.Controls.Add(this.btnLimpiar);
+            this.ribbonPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ribbonPanel1.Location = new System.Drawing.Point(0, 25);
+            this.ribbonPanel1.Name = "ribbonPanel1";
+            this.ribbonPanel1.Padding = new System.Windows.Forms.Padding(3, 0, 3, 3);
+            this.ribbonPanel1.Size = new System.Drawing.Size(761, 70);
+            // 
+            // 
+            // 
+            this.ribbonPanel1.Style.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            // 
+            // 
+            // 
+            this.ribbonPanel1.StyleMouseDown.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            // 
+            // 
+            // 
+            this.ribbonPanel1.StyleMouseOver.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.ribbonPanel1.TabIndex = 1;
+            // 
+            // ribbonBar4
+            // 
+            this.ribbonBar4.AutoOverflowEnabled = true;
+            // 
+            // 
+            // 
+            this.ribbonBar4.BackgroundMouseOverStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            // 
+            // 
+            // 
+            this.ribbonBar4.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.ribbonBar4.ContainerControlProcessDialogKey = true;
+            this.ribbonBar4.Dock = System.Windows.Forms.DockStyle.Left;
+            this.ribbonBar4.DragDropSupport = true;
+            this.ribbonBar4.Items.AddRange(new DevComponents.DotNetBar.BaseItem[] {
+            this.btnSalir});
+            this.ribbonBar4.LicenseKey = "F962CEC7-CD8F-4911-A9E9-CAB39962FC1F";
+            this.ribbonBar4.Location = new System.Drawing.Point(90, 0);
+            this.ribbonBar4.Name = "ribbonBar4";
+            this.ribbonBar4.Size = new System.Drawing.Size(83, 67);
+            this.ribbonBar4.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.ribbonBar4.TabIndex = 5;
+            // 
+            // 
+            // 
+            this.ribbonBar4.TitleStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            // 
+            // 
+            // 
+            this.ribbonBar4.TitleStyleMouseOver.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.AutoDisposeImages = true;
+            this.btnSalir.ButtonStyle = DevComponents.DotNetBar.eButtonStyle.ImageAndText;
+            this.btnSalir.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSalir.Image = ((System.Drawing.Image)(resources.GetObject("btnSalir.Image")));
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.SubItemsExpandWidth = 14;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.Tooltip = "Regresar al menú del módulo";
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.AutoOverflowEnabled = true;
+            // 
+            // 
+            // 
+            this.btnLimpiar.BackgroundMouseOverStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            // 
+            // 
+            // 
+            this.btnLimpiar.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.btnLimpiar.ContainerControlProcessDialogKey = true;
+            this.btnLimpiar.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnLimpiar.DragDropSupport = true;
+            this.btnLimpiar.Items.AddRange(new DevComponents.DotNetBar.BaseItem[] {
+            this.btnInicio});
+            this.btnLimpiar.LicenseKey = "F962CEC7-CD8F-4911-A9E9-CAB39962FC1F";
+            this.btnLimpiar.Location = new System.Drawing.Point(3, 0);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(87, 67);
+            this.btnLimpiar.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.btnLimpiar.TabIndex = 2;
+            // 
+            // 
+            // 
+            this.btnLimpiar.TitleStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            // 
+            // 
+            // 
+            this.btnLimpiar.TitleStyleMouseOver.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            // 
+            // btnInicio
+            // 
+            this.btnInicio.ButtonStyle = DevComponents.DotNetBar.eButtonStyle.ImageAndText;
+            this.btnInicio.Image = global::Textado.Properties.Resources.checkmark1;
+            this.btnInicio.Name = "btnInicio";
+            this.btnInicio.SubItemsExpandWidth = 14;
+            this.btnInicio.Text = "Iniciar";
+            this.btnInicio.Click += new System.EventHandler(this.btnInicio_Click);
+            // 
+            // ribbonTabItem2
+            // 
+            this.ribbonTabItem2.Checked = true;
+            this.ribbonTabItem2.Name = "ribbonTabItem2";
+            this.ribbonTabItem2.Panel = this.ribbonPanel1;
+            this.ribbonTabItem2.Text = "Opciones";
+            // 
+            // styleManager1
+            // 
+            this.styleManager1.ManagerStyle = DevComponents.DotNetBar.eStyle.Office2010Silver;
+            this.styleManager1.MetroColorParameters = new DevComponents.DotNetBar.Metro.ColorTables.MetroColorGeneratorParameters(System.Drawing.Color.White, System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(87)))), ((int)(((byte)(154))))));
+            // 
+            // frmTextado
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(761, 211);
+            this.Controls.Add(this.ribbonControl1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.circularProgress1);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBucar);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblNombre);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(777, 250);
+            this.MinimumSize = new System.Drawing.Size(777, 250);
+            this.Name = "frmTextado";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Textado";
+            this.Load += new System.EventHandler(this.frmTextado_Load);
+            this.ribbonControl1.ResumeLayout(false);
+            this.ribbonControl1.PerformLayout();
+            this.ribbonPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button btnBucar;
+        private System.Windows.Forms.OpenFileDialog ofdSeleccionar;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Label label1;
+        private DevComponents.DotNetBar.Controls.CircularProgress circularProgress1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private DevComponents.DotNetBar.RibbonControl ribbonControl1;
+        private DevComponents.DotNetBar.RibbonPanel ribbonPanel1;
+        private DevComponents.DotNetBar.RibbonBar ribbonBar4;
+        private DevComponents.DotNetBar.ButtonItem btnSalir;
+        private DevComponents.DotNetBar.RibbonBar btnLimpiar;
+        private DevComponents.DotNetBar.ButtonItem btnInicio;
+        private DevComponents.DotNetBar.RibbonTabItem ribbonTabItem2;
+        private DevComponents.DotNetBar.StyleManager styleManager1;
+    }
+}
+
---
+++ Textado/frmTextado.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Word = Microsoft.Office.Interop.Word;
+using System.Diagnostics;
+
+namespace Textado
+{
+    public partial class frmTextado : Form
+    {
+     
+        string Caracter;
+
+
+        public frmTextado()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBucar_Click(object sender, EventArgs e)
+        {
+
+            ofdSeleccionar.Filter = "Office Files (*.docx)|*.docx|All Files (*.*)|*.* "; 
+            ofdSeleccionar.RestoreDirectory = true;
+            ofdSeleccionar.Multiselect = false;
+
+            if (ofdSeleccionar.ShowDialog() == DialogResult.OK)
+            {
+                txtNombre.Text = ofdSeleccionar.FileName;
+                txtResultado.Clear();
+            }
+
+        }
+
+        private void BuscarHighLight(string pFileName = "")
+        {
+            Word.Application wordObject = new Word.Application();
+            Word.Document thisDoc;
+            try
+            {
+                object nullobject = System.Reflection.Missing.Value;
+                object file = pFileName;
+                thisDoc = wordObject.Documents.Open(ref file, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject);
+                string tempStrText = "", tempStr = "";
+                string FilePath = "", Name = "";
+                int thisStart = 0, thisEnd = 0;
+                thisDoc.Activate();
+                wordObject.Visible = false;
+
+                object missing = Type.Missing;
+
+                Word.Range objRange = thisDoc.Content;
+       
+                objRange.Find.Highlight = 1;
+                objRange.Find.Forward = true;
+
+                //objRange.Find.Text.Contains("");
+
+                bool found = objRange.Find.Execute("", missing, missing, missing, missing, missing, true,
+                    missing, missing, missing, missing, missing, missing, missing, missing);
+                if (found)
+                {
+                    circularProgress1.Visible = true;
+                    do
+                    {
+                        circularProgress1.IsRunning = true;
+                        string cellWordText = objRange.Text.Trim();
+
+                        if (objRange.HighlightColorIndex.ToString() == "wdYellow")
+                        {
+                            objRange.Select();
+                            objRange.HighlightColorIndex = Word.WdColorIndex.wdNoHighlight;
+                            String foo = cellWordText;
+                            String[] bar = foo.Split(' ');
+                            thisStart = objRange.Start;
+                            thisEnd = objRange.End;
+                            foreach (String foobar in bar)
+                            {
+                                int total = foobar.Length;
+                                for (int a = 0; a < total; a++)
+                                {
+                                    tempStrText = tempStrText + Caracter;
+
+                                }
+                                thisStart = thisStart;
+                                thisEnd = thisStart + total;
+                                ReplaceText(thisDoc, cellWordText, thisStart, thisEnd, tempStrText);
+                                thisStart = thisEnd + 1;
+                                tempStrText = "";
+                            }
+
+                           
+                            tempStr = "";
+                            tempStrText = "";
+                        }
+
+                        int intPosition = objRange.End;
+                        objRange.Start = intPosition;
+                    } while (objRange.Find.Execute("", missing, missing, missing, missing, missing, true,
+                        missing, missing, missing, missing, missing, missing, missing, missing));
+                }
+                buscar_Texto(thisDoc);
+                FilePath = System.IO.Path.GetDirectoryName(ofdSeleccionar.FileName);
+                Name = System.IO.Path.GetFileNameWithoutExtension(ofdSeleccionar.FileName);
+
+                int cont = 1;
+                string nombredic = "_Público", Extencion = ".docx", tmpnombredic = "";
+                tmpnombredic = nombredic;
+                while (System.IO.File.Exists(FilePath + @"\" + Name + tmpnombredic + Extencion))
+                {
+                   
+                    tmpnombredic = nombredic + "(" + cont.ToString() + ")";
+                    cont++;
+                }
+                nombredic = tmpnombredic;
+                Name = FilePath + @"\" + Name + nombredic + Extencion ;
+                txtResultado.Text = Name;
+                
+                thisDoc.SaveAs2(Name, nullobject, nullobject);
+             
+                wordObject.Quit(true, Type.Missing, Type.Missing);
+
+                circularProgress1.IsRunning = false;
+                circularProgress1.Visible = false;
+                if (MessageBox.Show("La versión pública del documentos se generó y guardó con éxito, " + System.Environment.NewLine + "¿Deseas abrir el directorio donde se guardó?", "Documento público guardado", MessageBoxButtons.OKCancel, MessageBoxIcon.Information) == DialogResult.OK)
+                {
+                    Process Proceso = Process.Start( FilePath);
+                }
+               
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+
+
+        private void ReplaceText( Word.Document pthisDoc, string pFind = "", int pStart = 0, int pEnd = 0, string pTexted = "")
+        {
+
+            object start = pStart;
+            object end = pEnd;
+            Word.Range rng = pthisDoc.Range(start, pEnd);
+            rng.Select();
+            rng.HighlightColorIndex = Word.WdColorIndex.wdNoHighlight;
+            rng.Find.ClearFormatting();
+            rng.Text = pTexted;
+
+        }
+
+        private void buscar_Texto(Word.Document pthisDoc)
+        {
+                object missing = Type.Missing;
+                object start = 1;
+                object end = 1;
+
+                Word.Range objRange = pthisDoc.Content;
+                objRange.Find.Text = "daño a la hacienda";
+                objRange.Find.Forward = true;
+
+
+                bool found = objRange.Find.Execute("daño a la hacienda", missing, missing, missing, missing, missing, true,
+                    missing, missing, missing, missing, missing, missing, missing, missing);
+                if (found)
+                {
+
+                    objRange.Select();
+                    objRange.HighlightColorIndex = Word.WdColorIndex.wdDarkRed;        
+                }
+        }
+
+        private void BuscarHighLight_old(string pFileName = "")
+        {
+            /*
+            try
+            {
+            int thisStart = 0;
+            int thisEnd = 0;
+            string tempStr = "", tempStrText = "", FilePath = "", Name = "";
+            object SaveChanges = Word.WdSaveOptions.wdSaveChanges;
+            object file = pFileName;
+            object nullobject = System.Reflection.Missing.Value;
+        
+            thisDoc = wordObject.Documents.Open(ref file, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject);
+
+            thisDoc.Activate();
+            wordObject.Visible = true;
+            
+            
+            Word.Range myRange;
+            myRange = thisDoc.Content;
+           // List<string> wordHighlights = new List<string>();
+            foreach (Word.Range cellWordRange in myRange.Words)
+            {
+                if (cellWordRange.HighlightColorIndex.ToString() == "wdNoHighlight")
+                {
+                    continue;
+                }
+                else
+                {
+                    thisStart = cellWordRange.Start;
+                    thisEnd = cellWordRange.End;
+                    string cellWordText = cellWordRange.Text.Trim();
+                    if (cellWordText.Length >= 1)   // valid word length, non-whitespace
+                    {
+                        int total = cellWordText.Length;
+                        for (int i = 0; i < total; i++)
+                        {
+                            tempStrText = tempStrText + "█";
+                        }
+                       
+                        ReplaceText(cellWordText, thisStart, thisEnd, tempStrText);
+                            tempStr = "";
+                            tempStrText = "";
+                    }
+                       
+                }
+            }
+
+       
+            FilePath = System.IO.Path.GetDirectoryName(ofdSeleccionar.FileName);
+            Name = System.IO.Path.GetFileNameWithoutExtension(ofdSeleccionar.FileName);
+            Name = FilePath + @"\" + Name + "_Textado.docx";
+            txtResultado.Text = FilePath + @"\" + Name + "_Textado.docx";
+            thisDoc.SaveAs2(Name, nullobject, nullobject);
+            
+            //thisDoc.Close(null, null, null);
+            wordObject.Quit();
+           
+                
+            }
+            catch (Exception j)
+            {
+                MessageBox.Show(j.Message);
+            }
+           
+        */
+        }
+
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Caracter = comboBox1.Text.Trim();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void frmTextado_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void btnInicio_Click(object sender, EventArgs e)
+        {
+            string FileName = this.txtNombre.Text.Trim();
+            if (!string.IsNullOrWhiteSpace(FileName))
+                BuscarHighLight(FileName);
+        }
+
+
+
+ 
+      
+
+    }
+
+  
+
+}
+
+
+
